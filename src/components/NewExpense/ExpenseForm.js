6799ea44cddaa539791843e57093d458b22a83b6
senyiyiyi <senyi0201@outlook.com>
2023-10-23 18:59:34 +0800

    712. Using Stateful Lists

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expencseData = {
       title: enteredTitle,
-      amoumt: enteredAmount,
+      amount: enteredAmount,
       date: new Date(enteredDate)
     };
     props.onSaveExpenseData(expencseData);
