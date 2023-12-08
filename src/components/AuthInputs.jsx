2f3fa81d41aacd5ac035de668c4f42291de3e278
senyiyiyi <senyi0201@outlook.com>
2023-12-08 18:20:16 +0800

    119. Tailwind: Media Queries & Pseudo Selectors

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -2,27 +2,13 @@ import { useState } from 'react';
 import { styled } from 'styled-components';
 
 import CustomInputs from './Inputs.jsx';
+import Button from './Button.jsx';
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
 `
 
 export default function AuthInputs() {
@@ -69,7 +55,8 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        {/* <Button className='button' onClick={handleLogin}>Sign In</Button> */}
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
