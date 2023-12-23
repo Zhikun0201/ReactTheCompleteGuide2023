ad1f8340170c0ce23244e3b74676534257c9d862
senyiyiyi <senyi0201@outlook.com>
2023-12-23 14:08:28 +0800

    第 9 节：Practice Project: Project Management App (with Components, State, Refs & More)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import Player from './components/Player.jsx';
-import TimerChallenge from './components/TimerChallenge.jsx';
-
 function App() {
   return (
     <>
-      <Player />
-      <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Normal" targetTime={5} />
-        <TimerChallenge title="Hard" targetTime={10} />
-        <TimerChallenge title="Very Hard" targetTime={15} />
-      </div>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
     </>
   );
 }
