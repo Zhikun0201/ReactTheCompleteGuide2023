06c40e2958fc89a75af1d0f704fa284ea0108b1c
senyiyiyi <senyi0201@outlook.com>
2023-12-23 14:05:29 +0800

    第 8 节：Working with Refs & Portals

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from 'react-dom'
 
 const resultModal = forwardRef(
     function ResultModal({ targetTime, remainingTime, onReset }, ref) {
@@ -17,20 +18,21 @@ const resultModal = forwardRef(
         })
 
         return (
-            <dialog ref={dialog} className="result-modal">
-                {userLost && <h2>You Lost!</h2>}
-                {!userLost && <h2>Your Score: {score}</h2>}
-                <p>
-                    The target time was <strong>{targetTime} seconds.</strong>
-                </p>
-                <p>
-                    You stopped the timer with{' '}
-                    <strong>{formattedRemainingTime} seconds left.</strong>
-                </p>
-                <form method="dialog" onSubmit={onReset}>
-                    <button>Close</button>
-                </form>
-            </dialog>
+            createPortal(
+                <dialog ref={dialog} className="result-modal">
+                    {userLost && <h2>You Lost!</h2>}
+                    {!userLost && <h2>Your Score: {score}</h2>}
+                    <p>
+                        The target time was <strong>{targetTime} seconds.</strong>
+                    </p>
+                    <p>
+                        You stopped the timer with{' '}
+                        <strong>{formattedRemainingTime} seconds left.</strong>
+                    </p>
+                    <form method="dialog" onSubmit={onReset}>
+                        <button>Close</button>
+                    </form>
+                </dialog>, document.getElementById('modal'))
         )
     })
 export default resultModal
