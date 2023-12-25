bce37c994c38df45ec4e40a349e610435157bc59
senyiyiyi <senyi0201@outlook.com>
2023-12-25 13:46:06 +0800

    172. Managing State to Switch Between Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
+import NewProject from "./components/sidebar/NewProject";
+import NoProjectSelected from "./components/sidebar/NoProjectSelected";
 import ProjectSidebar from "./components/sidebar/ProjectSidebar";
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+
+  function handleStartProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddPrject={handleStartProject} />;
+  }
+
   return (
-    <main>
-      <ProjectSidebar />
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar onStartAddPrject={handleStartProject}/>
+      {content}
     </main>
   );
 }
--- src/components/sidebar/ProjectSidebar.jsx
+++ src/components/sidebar/ProjectSidebar.jsx
@@ -1,9 +1,11 @@
-export default function ProjectSidebar() {
+import Button from "./Button";
+
+export default function ProjectSidebar({onStartAddPrject}) {
     return (
-        <aside>
-            <h2>Your Projects</h2>
+        <aside className="w-1/3 px-9 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
             <div>
-                <button>+ Add Project</button>
+                <Button onClick={onStartAddPrject}>+ Add Project</Button>
             </div>
             <ul>
             </ul>
