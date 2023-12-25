bcdea3e6c3eef7057424db200c0d749401d90900
senyiyiyi <senyi0201@outlook.com>
2023-12-25 11:51:02 +0800

    167. Adding a "Project Sidebar" Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import ProjectSidebar from "./components/sidebar/ProjectSidebar";
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main>
+      <ProjectSidebar />
+    </main>
   );
 }
 
