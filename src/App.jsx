739415ab86ba9f02f430773885bf6cdbe9e8b60a
Zhikun0201 <37325449+Zhikun0201@users.noreply.github.com>
2024-01-01 22:37:04 +0800

    186. Creating & Providing The Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import {DUMMY_PRODUCTS} from './dummy-products.js';
+import {CartContext} from "./store/shopping-cart-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -67,11 +68,15 @@ function App() {
 
   return (
     <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <CartContext.Provider
+        value={{items: []}}
+      >
+        <Header
+          cart={shoppingCart}
+          onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+        />
+        <Shop onAddItemToCart={handleAddItemToCart}/>
+      </CartContext.Provider>
     </>
   );
 }
