eb98391c19783fb98da66f09f6559e641c8f3799
senyiyiyi <senyi0201@outlook.com>
2024-01-09 20:47:32 +0800

    第 15 节：Sending Http Requests (e.g. Connecting to a Database)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState, useCallback} from 'react';
+import {useRef, useState, useCallback, useEffect} from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
@@ -6,15 +6,33 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 
-import {updateUserPlaces} from "./http.js";
+import {updateUserPlaces, fetchUserPlaces} from "./http.js";
 import Error from "./components/Error.jsx";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [fetchError, setFetchError] = useState(null); // [fetchError, setFetchError
   const [updatePlacesError, setUpdatePlacesError] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    async function handleFetchUserPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchUserPlaces();
+        setUserPlaces(places);
+      } catch (error) {
+        setFetchError(error.message || "Something went wrong!");
+      } finally {
+        setIsFetching(false);
+      }
+    }
+
+    handleFetchUserPlaces()
+  }, []);
 
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
@@ -26,6 +44,10 @@ function App() {
   }
 
   function handleSelectPlace(selectedPlace) {
+    if (userPlaces.some((place) => place.id === selectedPlace.id)) {
+      return;
+    }
+    
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -36,12 +58,11 @@ function App() {
       return [selectedPlace, ...prevPickedPlaces];
     });
 
-    const response = updateUserPlaces([selectedPlace, ...userPlaces])
+    updateUserPlaces([selectedPlace, ...userPlaces])
       .catch((error) => {
         setUserPlaces(userPlaces);
         setUpdatePlacesError(error.message || "Something went wrong!");
       })
-    console.log(response);
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
@@ -89,12 +110,18 @@ function App() {
         </p>
       </header>
       <main>
-        <Places
+        {fetchError && <Error
+          title="Could not fetch your places"
+          message={fetchError}
+        />}
+        {!fetchError && <Places
           title="I'd like to visit ..."
           fallbackText="Select the places you would like to visit below."
+          isLoading={isFetching}
+          loadingText={"Loading places..."}
           places={userPlaces}
           onSelectPlace={handleStartRemovePlace}
-        />
+        />}
 
         <AvailablePlaces onSelectPlace={handleSelectPlace}/>
       </main>
