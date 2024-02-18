0a6e64ee05f4da54192ca521280b5216f376850f
senyiyiyi <senyi0201@outlook.com>
2024-02-18 15:14:29 +0800

    245. Exposing Nested Functions From The Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
   }
 
   async function handleSelectPlace(selectedPlace) {
-    // await updateUserPlaces([selectedPlace, ...userPlaces]);
+    await updateUserPlaces([selectedPlace, ...userPlaces]);
 
     setUserPlaces((prevPickedPlaces) => {
       if (!prevPickedPlaces) {
