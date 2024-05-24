cbc08b036e1ec2aff96a2520319a0742e481cc81
senyiyiyi <senyi0201@outlook.com>
2024-05-24 12:23:53 +0800

    251. Managing & Getting User Input via State & Generic Handlers

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,23 @@
+import {useState} from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState(
+    {
+      email : '',
+      password : ''
+    })
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('Form submitted');
+    console.log("EnteredValues:", enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      // 使用了计算属性名
+      [identifier] : value
+    }))
   }
 
   return (
@@ -11,12 +27,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange('email', event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange('password', event.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
@@ -24,7 +52,8 @@ export default function Login() {
         <button className="button button-flat">Reset</button>
         <button
           className="button"
-        >Login</button>
+        >Login
+        </button>
       </p>
     </form>
   );
